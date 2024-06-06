c85184026cedd948849d23ffbef31e9091accc2a
KimInteger <kjs20151240@gmail.com>
2024-06-06 13:33:20 +0900

    부모클래스와 자식클래스를 생성

--- practice-1.js
+++ practice-1.js
@@ -1,1 +1,21 @@
-console.log('super를 연습해보자.')
+class Animal {
+  constructor(type,live){
+    this.type = type;
+    this.live = live;
+  };
+
+  move(){
+    console.log('I can move!');
+  }
+};
+
+class Bird extends Animal {
+  constructor(type,live,name) {
+    super(type,live);
+    this.name = name;
+  };
+
+  fly(){
+    console.log('I can fly!');
+  }
+};
